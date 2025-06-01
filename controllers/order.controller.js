5989ad794f3622b18a8398e22ad3d1670d91e7ff
cyberseris <zihlingchen@gmail.com>
2025-06-01 19:21:16 +0800

    新增取得所有訂單功能

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -36,58 +36,7 @@ const orderController = {
             message: "成功取得訂單",
             data: result
         })
-    },
-
-
-
-/*     async getOrderList(req, res, next){
-        const user_id = req.user.id
-        
-        const orderRepo = dataSource.getRepository('order')
-        const findOrder = await orderRepo.find({
-            select: ['id', 'order_number', 'final_amount', 'created_at'],
-            where:{ user_id: user_id }
-        })
-
-        console.log("================getOrderList findOrder================")
-        console.log(findOrder)
-        console.log("================getOrderList findOrder================")
-
-        if(!findOrder){
-            return res.status(200).json({
-                status: true,
-                message: "目前沒有訂單"
-            })      
-        }
-
-        const orderItemRepo = dataSource.getRepository('order_item')
-        const orderItemList = await orderItemRepo.find({
-            select: ['id'],
-            where: {order_id: findOrder.id},
-            relations: ['courses']
-        })
-        
-        console.log("================getOrderList orderItemList================")
-        console.log(orderItemList)
-        console.log("================getOrderList orderItemList================")
-
-        const course_name = orderItemList.map(item => item.courses.course_name)
-
-        console.log("================getOrderList course_name================")
-        console.log(course_name)
-        console.log("================getOrderList course_name================")
-
-        return res.status(200).json({
-            status: true,
-            message: "成功取得訂單",
-            data: {
-                order_number: findOrder.order_number,
-                course_name: course_name,
-                final_amount: findOrder.final_amount,
-                created_at: findOrder.created_at
-            }
-        })
-    }, */
+    }
 }
 
 module.exports = orderController
